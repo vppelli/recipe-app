c99ed3d32049d2dfdbaf4fb3fbd76d924a6f7ab2
Michael Pietroski <mpworkplace@gmail.com>
2024-09-02 13:59:18 -0400

    Edited settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -120,11 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS=[BASE_DIR / 'static']
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
 
