fc0773126e8eb5f84c2ee7031dd5c4428e57ec26
Michael Pietroski <mpworkplace@gmail.com>
2024-09-02 12:24:19 -0400

    Updated Allowed Hosts

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['recipe-mikes.herokuapp.com']
+ALLOWED_HOSTS = ['recipe-mikes-8a1f7a74a98b.herokuapp.com']
 
 
 # Application definition
