4a74c49064306a077494d4e47c677880e95330a6
Michael Pietroski <mpworkplace@gmail.com>
2024-09-02 13:21:55 -0400

    Updated Setting.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -124,9 +124,9 @@ STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
-MEDIA_ROOT= BASE_DIR / 'media'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
